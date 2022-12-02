61a5ef14b8a7b49f455134f3265ed5b436ef4308
Tien-Thinh <tranthuongtienthinh@gmail.com>
2022-12-02 13:03:18 +0100

    index js

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,77 @@
 
         var viz;
 
+        function setup(viz) {
+            viz.network.on("doubleClick", function (params) {
+                // Si on double clique, on a des infos sur le noeud et le lien
+                params.event = "[original event]";
+                if (params["nodes"].length == 1) {
+                    node_id = params["nodes"][0];
+                    data = viz.nodes.get(node_id);
+                    adresse = data.raw.properties.adresse;
+                    console.log(adresse)
+                    params["edges"].forEach(element => {
+                        viz.network.updateEdge(element, { length: 600 });
+                    });
+                    cypher = `MATCH n=((:Personne {adresse:'${adresse}'})-[t:transfert]-(:Personne)) RETURN n LIMIT 10`;
+                    viz.updateWithCypher(cypher);
+                } else {
+                    edge_id = params["edges"][0];
+                    data = viz.nodes.get(edge_id);
+                }
+            })
+            viz.network.on("click", function (params) {
+                // Si on clique, on a des infos sur le noeud et le lien
+                params.event = "[original event]";
+                if (params["nodes"].length == 1) {
+                    node_id = params["nodes"][0];
+                    data = viz.nodes.get(node_id);
+                } else {
+                    edge_id = params["edges"][0];
+                    data = viz.edges.get(edge_id);
+                }
+                console.log(params);
+                document.getElementById("contenu_info").innerText = JSON.stringify(
+                    data,
+                    null,
+                    4
+                );
+            })
+            // Désactiver le clique droit 
+            document.addEventListener('contextmenu', event => event.preventDefault());
+            viz.network.on("oncontext", function (params) {
+                console.log(params);
+                viz.network.deleteSelected();
+            });
+            function download() {
+                var dl = document.getElementById("download");
+                var image = document.getElementsByClassName("vis-network")[0].firstChild.toDataURL("image/png")
+                    .replace("image/png", "image/octet-stream");
+                dl.setAttribute("href", image);
+                //download.setAttribute("download","archive.png");
+            };
+            console.log("download");
+            
+
+
+        viz.network.once("stabilizationIterationsDone", function() {
+            console.log("done");
+         
+         }); 
+
+
+        }
+
+
+
+
+
         function draw() {
             console.log("draw")
             var config = {
                 containerId: "viz",
                 neo4j: {
-                    serverUrl: "bolt://34.77.218.69:7687/websocket",
+                    serverUrl: "bolt://34.77.218.69:7687",
                     serverUser: "neo4j",
                     serverPassword: "azerty"
                 },
@@ -73,63 +138,9 @@
 
 
 
-
-
-
         // after load
 
 
-setTimeout(function () {
-    viz.network.on("doubleClick", function (params) {
-        // Si on double clique, on a des infos sur le noeud et le lien
-        params.event = "[original event]";
-        if (params["nodes"].length == 1) {
-            node_id = params["nodes"][0];
-            data = viz.nodes.get(node_id);
-            adresse = data.raw.properties.adresse;
-            console.log(adresse)
-            params["edges"].forEach(element => {
-                viz.network.updateEdge(element, { length: 600 });
-            });
-            cypher = `MATCH n=((:Personne {adresse:'${adresse}'})-[t:transfert]-(:Personne)) RETURN n LIMIT 10`;
-            viz.updateWithCypher(cypher);
-        } else {
-            edge_id = params["edges"][0];
-            data = viz.nodes.get(edge_id);
-        }
-    })
-    viz.network.on("click", function (params) {
-        // Si on clique, on a des infos sur le noeud et le lien
-        params.event = "[original event]";
-        if (params["nodes"].length == 1) {
-            node_id = params["nodes"][0];
-            data = viz.nodes.get(node_id);
-        } else {
-            edge_id = params["edges"][0];
-            data = viz.edges.get(edge_id);
-        }
-        console.log(params);
-        document.getElementById("contenu_info").innerText = JSON.stringify(
-            data,
-            null,
-            4
-        );
-    })
-    // Désactiver le clique droit 
-    document.addEventListener('contextmenu', event => event.preventDefault());
-    viz.network.on("oncontext", function (params) {
-        console.log(params);
-        viz.network.deleteSelected();
-    });
-    function download() {
-        var dl = document.getElementById("download");
-        var image = document.getElementsByClassName("vis-network")[0].firstChild.toDataURL("image/png")
-            .replace("image/png", "image/octet-stream");
-        dl.setAttribute("href", image);
-        //download.setAttribute("download","archive.png");
-    };
-    console.log("download");
-}, 5000);
 
 
 
